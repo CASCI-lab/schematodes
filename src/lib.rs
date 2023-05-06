21a3001c21ac5faf205cd805def182b1de66a928
jcrozum <jcrozum@gmail.com>
2023-05-06 17:07:56 -0400

    better tree pruning

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
 use std::collections::HashSet;
 
-use pyo3::prelude::*;
 use itertools::Itertools;
+use pyo3::prelude::*;
 
 /// A Python module implemented in Rust.
 #[pymodule]
@@ -21,7 +21,10 @@ struct TwoSymbolSchema {
 impl TwoSymbolSchema {
     #[new]
     fn py_new(redescribed_schema: Vec<Vec<u8>>, bubble_indices: Vec<Vec<usize>>) -> PyResult<Self> {
-        Ok(Self{redescribed_schema, bubble_indices})
+        Ok(Self {
+            redescribed_schema,
+            bubble_indices,
+        })
     }
     #[getter]
     fn get_redescribed_schema(&self) -> PyResult<Vec<Vec<u8>>> {
@@ -33,41 +36,118 @@ impl TwoSymbolSchema {
     }
 }
 
+#[derive(Hash, Eq, PartialEq, Clone, Debug)]
+struct OneSymbolSubset {
+    schema: Vec<Vec<u8>>,
+    indices: Vec<usize>,
+    last_index_removed: Option<usize>,
+}
+
+impl OneSymbolSubset {
+    fn remove_index(&mut self, index_to_remove: usize) {
+        if let Some(index_position) = self.indices.iter().position(|i| *i == index_to_remove) {
+            self.schema.remove(index_position);
+            self.indices.remove(index_position);
+            self.last_index_removed = Some(index_to_remove);
+        } else {
+            panic!();
+        }
+    }
+}
+
 #[pyfunction]
 fn schemer(pis: Vec<Vec<u8>>) -> PyResult<Vec<TwoSymbolSchema>> {
     let mut found_sym: HashSet<Vec<usize>> = HashSet::new();
     let mut tss_vec: Vec<TwoSymbolSchema> = Vec::new();
-    for depth in 0..pis.len() {
-        let combo_indices = (0..pis.len()).combinations(pis.len()-depth);
-        let combo_group = pis.iter().combinations(pis.len()-depth);
-        
-        for (group, included_inds) in combo_group.zip(combo_indices) {
-            println!("group = {:?}",&group);
-            if combon_superset_seen(&included_inds, &found_sym){
-                continue
+    let mut next_depth_queue: HashSet<OneSymbolSubset> = HashSet::new();
+    let n_schema = pis.len();
+    next_depth_queue.insert(OneSymbolSubset {
+        schema: pis,
+        indices: (0..n_schema).collect(),
+        last_index_removed: None,
+    });
+
+    for _depth in 0..n_schema {
+        let depth_queue = next_depth_queue.clone();
+        next_depth_queue.clear();
+        // println!("depth_queue = {:?}", &depth_queue.len());
+
+        for oss in depth_queue {
+            let group = oss.schema.clone();
+            let included_inds = oss.indices.clone();
+            // at this point, group is a vector of one symbol schemata, each represented as a vector of 0s, 1s, and 2s,
+            // and included_inds is a vector of indices into pis that describe group.
+            // println!("group = {:?}", &group);
+            if combo_superset_seen(&included_inds, &found_sym) {
+                continue;
             }
-            // at this point, group is a vector of one symbol schemata, each represented as a vector of 0s, 1s, and 2s.
+
             let sym = check_for_symmetry(&group);
-            println!("sym = {:?}",&sym);
+
             if group.len() == 1 || !sym.is_empty() {
-                found_sym.insert(included_inds);
+                // symmetry to report or done
+                found_sym.insert(included_inds.clone());
                 tss_vec.push(TwoSymbolSchema {
                     redescribed_schema: group.iter().map(|x| x.to_vec()).collect(),
                     bubble_indices: sym,
                 })
+            } else {
+                for index_to_remove in included_inds {
+                    if let Some(last_index_removed) = oss.last_index_removed {
+                        // avoids duplicating groups
+                        if last_index_removed > index_to_remove {
+                            continue;
+                        }
+                        // println!("last_index_removed = {:?}", &last_index_removed);
+                    }
+                    // println!("index_to_remove = {:?}", &index_to_remove);
+
+                    next_depth_queue.insert({
+                        let mut new_oss = oss.clone();
+                        new_oss.remove_index(index_to_remove);
+                        new_oss
+                    });
+                }
             }
         }
     }
     Ok(tss_vec)
 }
 
-fn combon_superset_seen(included_inds: &Vec<usize>, found_sym: &HashSet<Vec<usize>>) -> bool {
+fn collapse_two_symbol_vector(tss_vec: &mut Vec<TwoSymbolSchema>) {
+    let n_tss = tss_vec.len();
+    if n_tss < 2 {
+        return;
+    }
+    let stake: usize = 0;
+    let cast: usize = 1;
+
+    // while stake < n_tss - 1 {}
+    todo!()
+}
+
+fn can_merge_two_symbol_schemas(tss1: &TwoSymbolSchema, tss2: &TwoSymbolSchema) -> bool {
+    todo!()
+}
+
+fn merge_two_symbol_schemas(tss1: &TwoSymbolSchema, tss2: &TwoSymbolSchema) -> TwoSymbolSchema {
+    let mut new_schema = tss1.redescribed_schema.clone();
+    new_schema.extend(tss2.redescribed_schema.clone());
+    let mut new_indices = tss1.bubble_indices.clone();
+    new_indices.extend(tss2.bubble_indices.clone());
+    TwoSymbolSchema {
+        redescribed_schema: new_schema,
+        bubble_indices: new_indices,
+    }
+}
+
+fn combo_superset_seen(included_inds: &Vec<usize>, found_sym: &HashSet<Vec<usize>>) -> bool {
     for seen in found_sym {
         let mut seen_matches = true;
         for i in included_inds {
-            if !seen.contains(i){
+            if !seen.contains(i) {
                 seen_matches = false;
-                break
+                break;
             }
         }
         if seen_matches {
@@ -77,39 +157,40 @@ fn combon_superset_seen(included_inds: &Vec<usize>, found_sym: &HashSet<Vec<usiz
     false
 }
 
-fn check_for_symmetry(group: &[&Vec<u8>]) -> Vec<Vec<usize>> {
+fn check_for_symmetry(group: &Vec<Vec<u8>>) -> Vec<Vec<usize>> {
     if group.len() <= 1 {
         return Vec::new();
     }
 
     let n_cols = group[0].len();
     let mut sym: Vec<Vec<usize>> = Vec::new();
-    let nontrivial_columns: Vec<usize> = (0..n_cols).filter( |i|group.iter().any(|x| x[*i] != group[0][*i])).collect();
-    
+    let nontrivial_columns: Vec<usize> = (0..n_cols)
+        .filter(|i| group.iter().any(|x| x[*i] != group[0][*i]))
+        .collect();
+
     let group_hash: HashSet<Vec<u8>> = group
         .iter()
         .map(|g| {
             let g_iter: Vec<u8> = g
-            .iter().enumerate()
-            .filter(|(i,_)| nontrivial_columns.contains(i)) 
-            .map( |(_,x)| *x)
-            .collect();
+                .iter()
+                .enumerate()
+                .filter(|(i, _)| nontrivial_columns.contains(i))
+                .map(|(_, x)| *x)
+                .collect();
             g_iter.to_vec()
-
-        }
-        )
+        })
         .collect();
-    
+
     let group_hash_swap_two: HashSet<Vec<u8>> = group_hash
         .iter()
         .cloned()
         .map(|x| {
             let mut y = x;
-            y.swap(0,1);
+            y.swap(0, 1);
             y
         })
         .collect();
-    
+
     let group_hash_full_cycle: HashSet<Vec<u8>> = group_hash
         .iter()
         .map(|x| {
@@ -118,16 +199,10 @@ fn check_for_symmetry(group: &[&Vec<u8>]) -> Vec<Vec<usize>> {
             y
         })
         .collect();
-    println!("group input = {:?}",&group);
-    println!("nontrivial_columns = {:?}",&nontrivial_columns);
-    println!("group_hash = {:?}",&group_hash);
-    println!("group_hash_swap_two = {:?}",&group_hash_swap_two);
-    println!("group_hash_full_cycle = {:?}",&group_hash_full_cycle);
-    if group_hash == group_hash_swap_two && group_hash == group_hash_full_cycle{
+
+    if group_hash == group_hash_swap_two && group_hash == group_hash_full_cycle {
         sym = vec![nontrivial_columns]
     }
-
-
 
     sym
 }
